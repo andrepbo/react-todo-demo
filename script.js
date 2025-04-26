a41b9d261f19e3f958f7d0824102b1ab60bf61d4
Andre Oliveira <andre.pbo@gmail.com>
2025-04-26 13:39:38 +0100

    refactor: update title and enhance TaskList component with remove functionality

--- script.js
+++
@@ -1,9 +0,0 @@
-function addTask() {
-    const input = document.getElementById('task');
-    const list = document.getElementById('list');
-    const newTask = document.createElement('li');
-
-    newTask.textContent = input.value;
-    list.appendChild(newTask);
-    input.value = '';
-  }
